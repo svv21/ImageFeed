6805926e8472f85648603d62ad0b062dd23d04a4
Vladislava Scherbo <vladislava.volokhovich@gmail.com>
2025-05-22 14:33:54 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Vladislava Scherbo on 22.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
