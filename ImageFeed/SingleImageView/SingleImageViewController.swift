3bde54334f2d8bffc6a5c2458274bcc4424c1e74
Vladislava Scherbo <vladislava.volokhovich@gmail.com>
2025-06-09 11:11:54 +0300

    Настроен модальный способ презентации изображений(настроен переход к одному изображению и обратно - к ленте с изображениями)

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -14,4 +14,10 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         imageView.image = image
     }
+    
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
 }
+
